454f136576bd6deaf0153ddee3575f4076acd508
ClementOrn <marjoriejenningsc2460308925@outlook.com>
2024-03-23 03:04:55 +0800

    ea

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  base: '/vue-glide/',
+  title: 'Vue Glide',
+  editLinks: true,
+  description: 'Vue component on top of the Glide.js',
+  ga: 'UA-56182454-7',
+  themeConfig: {
+    nav: [
+      {
+        text: 'Other project',
+        items: [
+          {
+            text: 'Vue Eva Icons',
+            link: 'https://github.com/antonreshetov/vue-eva-icons'
+          },
+          { text: 'Email signature generator', link: 'https://mysigmail.com' }
+        ]
+      },
+      { text: 'GitHub', link: 'https://github.com/antonreshetov/vue-glide' }
+    ],
+    sidebar: ['/introduce/', '/getting-started/', '/props/', '/v-model/']
+  }
+}
